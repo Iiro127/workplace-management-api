f1dc8a1651e62887bccf205f97163b331376f77f
Iiro Anttila <iiro.a.anttila@gmail.com>
2024-11-14 19:16:25 +0200

    Added ProjectsApi and ProjectsController

---
+++ src/main/kotlin/com/example/api/ProjectsApi.kt
@@ -0,0 +1,9 @@
+package com.example.api
+
+import jakarta.inject.Inject
+import src.gen.java.org.openapitools.api.ProjectsApi
+import src.gen.java.org.openapitools.model.Project
+
+class ProjectsApi: ProjectsApi {
+
+}
