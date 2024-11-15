4f8adcf77b8a7b8cb7425072cf9717dbae24a75d
Iiro Anttila <iiro.a.anttila@gmail.com>
2024-11-15 13:43:02 +0200

    Added test for /system/ping GET endpoint

--- src/test/kotlin/com/example/SystemTest.kt
+++ src/test/kotlin/com/example/SystemTest.kt
@@ -3,18 +3,25 @@ package com.example
 import io.restassured.RestAssured
 import io.smallrye.common.constraint.Assert.assertTrue
 import org.hamcrest.CoreMatchers
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
 class SystemTest {
 
     @Test
     fun testPing(){
-        /*RestAssured.given()
-            .`when`().get("/Ping")
+        RestAssured.given()
+            .`when`().get("/system/ping")
             .then()
             .statusCode(200)
             .body(CoreMatchers.`is`("Pong"))
-    }*/
-        assertTrue(true)
+    }
+    companion object {
+        @JvmStatic
+        @BeforeAll
+        fun setup() {
+            RestAssured.baseURI = "http://localhost"
+            RestAssured.port = 8081
+        }
     }
 }
