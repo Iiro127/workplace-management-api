ea856087aedc53b1d5422747731ac57fa2c787d1
Iiro Anttila <iiro.a.anttila@gmail.com>
2024-12-13 13:05:34 +0200

    All done

--- src/test/kotlin/com/example/UsersTest.kt
+++ src/test/kotlin/com/example/UsersTest.kt
@@ -4,7 +4,6 @@ import com.example.db.UsersDatabaseController
 import data.MockUserData
 import io.restassured.RestAssured
 import org.bson.Document
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
@@ -23,6 +22,25 @@ class UsersTest: MockUserData() {
         assertNotNull(addedUser)
         assertEquals("Markku", addedUser?.getString("firstName"))
         assertTrue(addedUser!!.getBoolean("isActive"))
+    }
+
+    @Test
+    fun testUpdateUser() {
+        val initialUser = getUser().id("123123")
+        val updatedUser = initialUser.firstName("Updated")
+
+        databaseController.addUserToDatabase(initialUser)
+        val updateResult = databaseController.updateUserInDatabase("123123", updatedUser)
+
+        assertTrue(updateResult)
+
+        val updatedUserDocument = databaseController.getDatabase()
+            .find(Document("user.id", "123123"))
+            .first()
+            ?.get("user", Document::class.java)
+
+        assertNotNull(updatedUserDocument)
+        assertEquals("Updated", updatedUserDocument?.getString("firstName"))
     }
 
     companion object {
